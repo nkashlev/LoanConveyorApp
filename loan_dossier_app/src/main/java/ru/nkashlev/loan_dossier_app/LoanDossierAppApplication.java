4e2037d85fca3ad1a03d88684a963c34545bc929
nkashlev <kashlev-1996@mail.ru>
2023-09-06 21:45:12 +0300

    initial loan_dossier_app

---
+++ loan_dossier_app/src/main/java/ru/nkashlev/loan_dossier_app/LoanDossierAppApplication.java
@@ -0,0 +1,13 @@
+package ru.nkashlev.loan_dossier_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanDossierAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanDossierAppApplication.class, args);
+	}
+
+}
