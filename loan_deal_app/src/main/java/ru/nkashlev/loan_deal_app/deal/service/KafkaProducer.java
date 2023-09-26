39ccc0c231845d4313d04b89474b8247204e33a4
nkashlev <kashlev-1996@mail.ru>
2023-09-26 20:19:09 +0300

    delete constructor

--- loan_deal_app/src/main/java/ru/nkashlev/loan_deal_app/deal/service/KafkaProducer.java
+++ loan_deal_app/src/main/java/ru/nkashlev/loan_deal_app/deal/service/KafkaProducer.java
@@ -1,7 +1,6 @@
 package ru.nkashlev.loan_deal_app.deal.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import ru.nkashlev.loan_deal_app.deal.entity.util.EmailMessage;
@@ -9,13 +8,8 @@ import ru.nkashlev.loan_deal_app.deal.entity.util.EmailMessage;
 @Service
 @RequiredArgsConstructor
 public class KafkaProducer {
-    private KafkaTemplate<String, EmailMessage> kafkaTemplate;
 
-    @Autowired
-    public KafkaProducer(KafkaTemplate<String, EmailMessage> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
-
+    private final KafkaTemplate<String, EmailMessage> kafkaTemplate;
     public void sendMessage(String topic, EmailMessage message) {
         kafkaTemplate.send(topic, message);
     }
