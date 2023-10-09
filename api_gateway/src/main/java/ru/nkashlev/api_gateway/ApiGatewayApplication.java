7a6782f14b7c9a4cb19a9c0d76d9c4d8a501b9a8
nkashlev <kashlev-1996@mail.ru>
2023-10-09 22:50:48 +0300

    initial api_gateway

--- api_gateway/src/main/java/ru/nkashlev/api_gateway/ApiGatewayApplication.java
+++ api_gateway/src/main/java/ru/nkashlev/api_gateway/ApiGatewayApplication.java
@@ -1,9 +1,12 @@
-package ru.nkashlev;
+package ru.nkashlev.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
+
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
---
+++ api_gateway/src/main/java/ru/nkashlev/api_gateway/gateway/service/DealFeignClient.java
@@ -0,0 +1,8 @@
+//package ru.nkashlev.api_gateway.gateway.service;
+//
+//import org.springframework.cloud.openfeign.FeignClient;
+//
+//@FeignClient(name = "dealFeignClient", url = "http://localhost:8080/deal")
+//public interface DealFeignClient {
+//
+//}
